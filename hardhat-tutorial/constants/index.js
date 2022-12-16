150222831cce86ab8df75e0f08999d0d5b6c0e2f
DiyaaDaoud <didoodidoo2312@gmail.com>
2022-12-17 00:11:53 +0300

    adding vercel domain

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x1ac45C5fC47E52AEc600E35dB408fcB8823Cc473";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-marketplace-learn-web3-dao.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
